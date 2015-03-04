879077d664403ef12d0d4e0e2342d5634f787fbe
Pavlo Morozov <pavelmorozov@mail.ru>
2015-03-04 18:14:08 +0200

    back to main

--- src/main/java/jobpost/Jobpost.java
+++ src/main/java/jobpost/Jobpost.java
@@ -1,11 +1,29 @@
 package jobpost;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import jobpost.intel.Engine;
 
-public class Jobpost {
+public class Jobpost extends Application{
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }	
+	
 	public static void main(String[] args) {
 		//UI
+		launch(args);
+		
 		
 		// Starts application engine
 		Engine appEngine = new Engine(); 
