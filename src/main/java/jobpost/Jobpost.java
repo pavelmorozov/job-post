eda2f2c3d9c68b62ad6d2ad000ad5801d0ba4583
Pavlo Morozov <pavelmorozov@mail.ru>
2015-03-04 18:43:21 +0200

    Proceed with UI

--- src/main/java/jobpost/Jobpost.java
+++ src/main/java/jobpost/Jobpost.java
@@ -12,7 +12,7 @@ public class Jobpost extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MainForm.fxml"));
         
         Scene scene = new Scene(root);
         
---
+++ src/main/java/jobpost/ui/MainController.java
@@ -0,0 +1,19 @@
+package jobpost.ui;
+
+import javafx.fxml.Initializable;
+
+public class MainController implements Initializable{
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    }
