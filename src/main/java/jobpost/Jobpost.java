dd2d465bd6f9321739aef70098fdd178b4071f01
Pavlo Morozov <pavelmorozov@mail.ru>
2015-03-04 17:47:56 +0200

    test new branch - UI

--- src/main/java/jobpost/Jobpost.java
+++ src/main/java/jobpost/Jobpost.java
@@ -5,6 +5,8 @@ import jobpost.intel.Engine;
 public class Jobpost {
 
 	public static void main(String[] args) {
+		//UI
+		
 		// Starts application engine
 		Engine appEngine = new Engine(); 
 		appEngine.postJobs();
